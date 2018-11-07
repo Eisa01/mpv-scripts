7d302ef4db542b7ecfc3dc9ff660a165607b9fcb
Eisa01 <alawadhi.e.m@gmail.com>
2018-11-07 20:26:13 +0400

    Update and rename smartHistory-1.0.lua to smartHistory-1.1.lua

--- scripts/smartHistory-1.1.lua
+++ scripts/smartHistory-1.1.lua
@@ -1,10 +1,8 @@
-local copyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
-local copyLogOpen = io.open(copyLog, 'a+')
 local utils = require 'mp.utils'
-local seconds = 0
-local time = 0
 
 mp.register_event('file-loaded', function()
+	local seconds = 0
+	local time = 0
 	filePath = mp.get_property('path')
 
 	timer = mp.add_periodic_timer(1, function()
@@ -31,6 +29,8 @@ mp.register_event('unpause', function()
 end)
 
 mp.register_event('end-file', function()
+	local historyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
+	local historyLogAdd = io.open(historyLog, 'a+')
 	
 	if string.match(seconds, '-1') then
 		seconds = 0
@@ -38,13 +38,15 @@ mp.register_event('end-file', function()
 		seconds = seconds
 	end
 	
-	copyLogOpen:write(('[%s] %s\n'):format(os.date('%d/%b/%y %X'), filePath..'&t='..tostring(seconds)))
+	historyLogAdd:write(('[%s] %s\n'):format(os.date('%d/%b/%y %X'), filePath..'&t='..tostring(seconds)))
 	timer:kill()
+	historyLogAdd:close()
 end)
 
 mp.add_key_binding("ctrl+r", "resume", function()
-	local copyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
-	local copyLogOpen = io.open(copyLog, 'r')
+	local historyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
+	local historyLogOpen = io.open(historyLog, 'r')
+	local historyLogAdd = io.open(historyLog, 'a+')
 	local filePath = mp.get_property('path')
 	local linePosition
 	local videoFound
@@ -52,24 +54,28 @@ mp.add_key_binding("ctrl+r", "resume", function()
 	local currentVideoTime
 	
 	if (filePath ~= nil) then
-		for line in copyLogOpen:lines() do
+		for line in historyLogOpen:lines() do
 		   
 		   linePosition = line:find(']')
 		   line = line:sub(linePosition + 2)
 		   
-			if line.match(line, "(.*)&t=") == filePath then
+			if line.match(line, '(.*)&t=') == filePath then
 				videoFound = line
 			end
 		   
 		end
-
-		currentVideo = string.match(videoFound, "(.*)&t=")	
-		currentVideoTime = string.match(videoFound, "&t=(.*)")
+		
+	if (videoFound ~= nil) then
+		currentVideo = string.match(videoFound, '(.*)&t=')	
+		currentVideoTime = string.match(videoFound, '&t=(.*)')
 
 		if (filePath == currentVideo) and (currentVideoTime ~= nil) then
 			mp.commandv('seek', currentVideoTime, 'absolute', 'exact')
-			mp.osd_message("Resumed To Last Position")
-		end	
+			mp.osd_message('Resumed To Last Position')
+		end
+	else
+		mp.osd_message('No Resume Position in This Video')
+	end
 	else
 		local res = utils.subprocess({ args = {
         'powershell', '-NoProfile', '-Command', [[& {
@@ -80,40 +86,46 @@ mp.add_key_binding("ctrl+r", "resume", function()
 		Invoke-Item "$env:APPDATA\mpv\mpvHistory.log"
         }]]
     } })
-	
 		if not res.error then
 			return res.stdout
 		end
 		return false
 	end
+	historyLogAdd:close()
+	historyLogOpen:close()
 end)
 
 mp.add_key_binding("ctrl+l", "lastplay", function()
-	local copyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
-	local copyLogOpen = io.open(copyLog, 'r+')
+	local historyLog = os.getenv('APPDATA')..'/mpv/mpvHistory.log'
+	local historyLogAdd = io.open(historyLog, 'a+')
+	local historyLogOpen = io.open(historyLog, 'r+')
     local linePosition
 	local videoFile
 
-	for line in copyLogOpen:lines() do
+	for line in historyLogOpen:lines() do
 		lastVideoFound = line
 	end
-	
-	copyLogOpen:close()
+	historyLogAdd:close()
+	historyLogOpen:close()
 
-    linePosition = lastVideoFound:find(']')
-    lastVideoFound = lastVideoFound:sub(linePosition + 2)
-
-	if string.match(lastVideoFound, "(.*)&t=") then
-		videoFile = string.match(lastVideoFound, "(.*)&t=")
+	if (lastVideoFound ~= nil) then
+		linePosition = lastVideoFound:find(']')
+		lastVideoFound = lastVideoFound:sub(linePosition + 2)
+		
+		if string.match(lastVideoFound, '(.*)&t=') then
+			videoFile = string.match(lastVideoFound, '(.*)&t=')
+		else
+			videoFile = lastVideoFound
+		end
+		
+		if (filePath ~= nil) then
+			mp.commandv('loadfile', videoFile, 'append-play')
+			mp.osd_message('Added Last Video Into Playlist:\n'..videoFile)
+		else
+			mp.commandv('loadfile', videoFile)
+			mp.osd_message('Opened Last Video From History:\n'..videoFile)
+		end
 	else
-		videoFile = lastVideoFound
-	end
-	
-	if (filePath ~= nil) then
-		mp.commandv('loadfile', videoFile, 'append-play')
-		mp.osd_message("Added Last Video Into Playlist:\n"..videoFile)
-	else
-		mp.commandv('loadfile', videoFile)
-		mp.osd_message("Opened Last Video From History:\n"..videoFile)
+		mp.osd_message('No Video Found in History')
 	end
 end)
