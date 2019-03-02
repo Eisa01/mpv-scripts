3295e5e44bcb0c1de4fdfb60ae54f86d6b5a78f5
Eisa01 <alawadhi.e.m@gmail.com>
2019-03-02 14:30:57 +0400

    Update and rename SimpleUndo-2.2.lua to SimpleUndo-2.3.lua

--- scripts/SimpleUndo-2.3.lua
+++ scripts/SimpleUndo-2.3.lua
@@ -3,7 +3,6 @@ local seconds = 0
 local countTimer = 0
 local previousUndoTime = 0
 local undoTime = 0
-local seekNumber = 0
 
 mp.register_event('file-loaded', function()
 	filePath = mp.get_property('path')
@@ -13,20 +12,16 @@ mp.register_event('file-loaded', function()
 	end)
 	
 	timer2 = mp.add_periodic_timer(0.1, function()
+	
 		countTimer = countTimer + 0.1
 		
-		if countTimer == 0.6 then
-	
+		if (countTimer == 0.6) then
 			previousUndoTime = undoTime
 			undoTime = math.floor(mp.get_property_number('time-pos'))
 		
-			seekNumber = 1
-		
-			if (undoTime ~= previousUndoTime) and (seekNumber > 0) then
-				seconds = seconds - 0.5 --almost decrease as the countTimer makes better for accuracy
-				previousUndoTime = previousUndoTime + seconds
-				seconds = 0
-			end
+			seconds = seconds - 0.5
+			previousUndoTime = previousUndoTime + seconds
+			seconds = 0
 		end
 		
 	end)
@@ -50,18 +45,17 @@ end)
 mp.register_event('end-file', function()
 	timer:kill()
 	timer2:kill()
-	seekNumber = 0
 	previousUndoTime = 0
 	undoTime = 0
 end)
 
 mp.add_key_binding("ctrl+z", "undo", function()
-	if (filePath ~= nil) and (seekNumber > 0) and (countTimer > 0.5) then
+	if (filePath ~= nil) and (countTimer > 0.5) then
 		mp.commandv('seek', previousUndoTime, 'absolute', 'exact')
 		mp.osd_message('Undo Last Seek')
-	elseif (filePath ~= nil) and (seekNumber > 0) and (countTimer <= 0.5) then
+	elseif (filePath ~= nil) and (countTimer > 0) and (countTimer < 0.6) then
 		mp.osd_message('Seeking Still Running')
-	elseif (filePath ~= nil) and (seekNumber == 0) then
+	elseif (filePath ~= nil) and (countTimer == 0) then
 		mp.osd_message('No Undo Found')
 	end
 end)
