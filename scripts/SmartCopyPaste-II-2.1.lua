0262c4a9438af44047b4928544659a03810d46c2
Eisa01 <alawadhi.e.m@gmail.com>
2020-02-11 00:32:44 +0400

    Update SmartCopyPaste-II-2.1.lua

--- scripts/SmartCopyPaste-II-2.1.lua
+++ scripts/SmartCopyPaste-II-2.1.lua
@@ -3,22 +3,48 @@
 
 -- Creator: Eisa AlAwadhi
 -- Project: SmartCopyPaste-II
--- Version: 2.1
+-- Version: 2.2
 
-local device = nil --nil is for automatic device detection, or manually change to: 'windows' or 'mac' or 'linux'
+local utils = require 'mp.utils'
+local msg = require 'mp.msg'
+local protocols
+local extensions
+local pasted = false
 
-local protocols = { --add below (after a comma) any protocol you want SmartCopyPaste to work with; e.g: ,'ftp://'
-	'https?://' ,'magnet:'
-}
+----------------------------USER CUSTOMIZATION SETTINGS-----------------------------------
+--These settings are for users to manually change some options in the script.
+--Keybinds can be defined in the bottom of the script.
 
-local extensions = { --add below (after a comma) any extension you want SmartCopyPaste to work with; e.g: ,'pdf'
-	--video & audio
-    'ac3', 'a52', 'eac3', 'mlp', 'dts', 'dts-hd', 'dtshd', 'true-hd', 'thd', 'truehd', 'thd+ac3', 'tta', 'pcm', 'wav', 'aiff', 'aif',  'aifc', 'amr', 'awb', 'au', 'snd', 'lpcm', 'yuv', 'y4m', 'ape', 'wv', 'shn', 'm2ts', 'm2t', 'mts', 'mtv', 'ts', 'tsv', 'tsa', 'tts', 'trp', 'adts', 'adt', 'mpa', 'm1a', 'm2a', 'mp1', 'mp2', 'mp3', 'mpeg', 'mpg', 'mpe', 'mpeg2', 'm1v', 'm2v', 'mp2v', 'mpv', 'mpv2', 'mod', 'tod', 'vob', 'vro', 'evob', 'evo', 'mpeg4', 'm4v', 'mp4', 'mp4v', 'mpg4', 'm4a', 'aac', 'h264', 'avc', 'x264', '264', 'hevc', 'h265', 'x265', '265', 'flac', 'oga', 'ogg', 'opus', 'spx', 'ogv', 'ogm', 'ogx', 'mkv', 'mk3d', 'mka', 'webm', 'weba', 'avi', 'vfw', 'divx', '3iv', 'xvid', 'nut', 'flic', 'fli', 'flc', 'nsv', 'gxf', 'mxf', 'wma', 'wm', 'wmv', 'asf', 'dvr-ms', 'dvr', 'wtv', 'dv', 'hdv', 'flv','f4v', 'f4a', 'qt', 'mov', 'hdmov', 'rm', 'rmvb', 'ra', 'ram', '3ga', '3ga2', '3gpp', '3gp', '3gp2', '3g2', 'ay', 'gbs', 'gym', 'hes', 'kss', 'nsf', 'nsfe', 'sap', 'spc', 'vgm', 'vgz', 'm3u', 'm3u8', 'pls', 'cue',
-	--images
-	"ase", "art", "bmp", "blp", "cd5", "cit", "cpt", "cr2", "cut", "dds", "dib", "djvu", "egt", "exif", "gif", "gpl", "grf", "icns", "ico", "iff", "jng", "jpeg", "jpg", "jfif", "jp2", "jps", "lbm", "max", "miff", "mng", "msp", "nitf", "ota", "pbm", "pc1", "pc2", "pc3", "pcf", "pcx", "pdn", "pgm", "PI1", "PI2", "PI3", "pict", "pct", "pnm", "pns", "ppm", "psb", "psd", "pdd", "psp", "px", "pxm", "pxr", "qfx", "raw", "rle", "sct", "sgi", "rgb", "int", "bw", "tga", "tiff", "tif", "vtf", "xbm", "xcf", "xpm", "3dv", "amf", "ai", "awg", "cgm", "cdr", "cmx", "dxf", "e2d", "egt", "eps", "fs", "gbr", "odg", "svg", "stl", "vrml", "x3d", "sxd", "v2d", "vnd", "wmf", "emf", "art", "xar", "png", "webp", "jxr", "hdp", "wdp", "cur", "ecw", "iff", "lbm", "liff", "nrrd", "pam", "pcx", "pgf", "sgi", "rgb", "rgba", "bw", "int", "inta", "sid", "ras", "sun", "tga",
-	--other types
-	'torrent'
-}
+local device = nil --nil is for automatic device detection, OR manually change to: 'windows' or 'mac' or 'linux'
+
+local linux_copy = 'xclip -silent -selection clipboard -in' --copy command that will be used in Linux. OR write a different command
+local linux_paste = 'xclip -selection clipboard -o' --paste command that will be used in Linux. OR write a different command
+
+local mac_copy = 'pbcopy' --copy command that will be used in MAC. OR write a different command
+local mac_paste = 'pbpaste' --paste command that will be used in MAC. OR write a different command
+
+local windows_copy = 'powershell' --'powershell' is for using windows powershell to copy. OR write the copy command, e.g: ' clip'
+local windows_paste = 'powershell' --'powershell' is for using windows powershell to paste. OR write the paste command
+
+local paste_anything = false --false is for specific paste based on the specified extensions and protocols. OR change to true so paste accepts anything (not recommended to change this).
+
+if not paste_anything then
+	protocols = { --add below (after a comma) any protocol you want SmartCopyPaste to work with; e.g: ,'ftp://'
+		'https?://' ,'magnet:'
+	}
+	extensions = { --add below (after a comma) any extension you want SmartCopyPaste to work with; e.g: ,'pdf'
+		--video & audio
+		'ac3', 'a52', 'eac3', 'mlp', 'dts', 'dts-hd', 'dtshd', 'true-hd', 'thd', 'truehd', 'thd+ac3', 'tta', 'pcm', 'wav', 'aiff', 'aif',  'aifc', 'amr', 'awb', 'au', 'snd', 'lpcm', 'yuv', 'y4m', 'ape', 'wv', 'shn', 'm2ts', 'm2t', 'mts', 'mtv', 'ts', 'tsv', 'tsa', 'tts', 'trp', 'adts', 'adt', 'mpa', 'm1a', 'm2a', 'mp1', 'mp2', 'mp3', 'mpeg', 'mpg', 'mpe', 'mpeg2', 'm1v', 'm2v', 'mp2v', 'mpv', 'mpv2', 'mod', 'tod', 'vob', 'vro', 'evob', 'evo', 'mpeg4', 'm4v', 'mp4', 'mp4v', 'mpg4', 'm4a', 'aac', 'h264', 'avc', 'x264', '264', 'hevc', 'h265', 'x265', '265', 'flac', 'oga', 'ogg', 'opus', 'spx', 'ogv', 'ogm', 'ogx', 'mkv', 'mk3d', 'mka', 'webm', 'weba', 'avi', 'vfw', 'divx', '3iv', 'xvid', 'nut', 'flic', 'fli', 'flc', 'nsv', 'gxf', 'mxf', 'wma', 'wm', 'wmv', 'asf', 'dvr-ms', 'dvr', 'wtv', 'dv', 'hdv', 'flv','f4v', 'f4a', 'qt', 'mov', 'hdmov', 'rm', 'rmvb', 'ra', 'ram', '3ga', '3ga2', '3gpp', '3gp', '3gp2', '3g2', 'ay', 'gbs', 'gym', 'hes', 'kss', 'nsf', 'nsfe', 'sap', 'spc', 'vgm', 'vgz', 'm3u', 'm3u8', 'pls', 'cue',
+		--images
+		"ase", "art", "bmp", "blp", "cd5", "cit", "cpt", "cr2", "cut", "dds", "dib", "djvu", "egt", "exif", "gif", "gpl", "grf", "icns", "ico", "iff", "jng", "jpeg", "jpg", "jfif", "jp2", "jps", "lbm", "max", "miff", "mng", "msp", "nitf", "ota", "pbm", "pc1", "pc2", "pc3", "pcf", "pcx", "pdn", "pgm", "PI1", "PI2", "PI3", "pict", "pct", "pnm", "pns", "ppm", "psb", "psd", "pdd", "psp", "px", "pxm", "pxr", "qfx", "raw", "rle", "sct", "sgi", "rgb", "int", "bw", "tga", "tiff", "tif", "vtf", "xbm", "xcf", "xpm", "3dv", "amf", "ai", "awg", "cgm", "cdr", "cmx", "dxf", "e2d", "egt", "eps", "fs", "gbr", "odg", "svg", "stl", "vrml", "x3d", "sxd", "v2d", "vnd", "wmf", "emf", "art", "xar", "png", "webp", "jxr", "hdp", "wdp", "cur", "ecw", "iff", "lbm", "liff", "nrrd", "pam", "pcx", "pgf", "sgi", "rgb", "rgba", "bw", "int", "inta", "sid", "ras", "sun", "tga",
+		--other types
+		'torrent'
+	}
+---------------------------END OF USER CUSTOMIZATION SETTINGS------------------------
+else
+	protocols = {''}
+	extensions = {''}
+end
 
 if not device then
 	if os.getenv('windir') ~= nil then
@@ -30,12 +56,9 @@ if not device then
   end
 end
 
-local utils = require 'mp.utils'
-local msg = require 'mp.msg'
-
 function handleres(res, args)
   if not res.error and res.status == 0 then
-      return res.stdout
+	return res.stdout
   else
     msg.error("There was an error getting "..device.." clipboard: ")
     msg.error("  Status: "..(res.status or ""))
@@ -46,8 +69,16 @@ function handleres(res, args)
   end
 end
 
-
-local pasted = false
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
 
 local function get_extension(path)
     match = string.match(path, '%.([^%.]+)$' )
@@ -88,32 +119,37 @@ end
 
 
 function get_clipboard()
+local clip
   if device == 'linux' then
-    local args = { 'xclip', '-selection', 'clipboard', '-o' }
-    return handleres(utils.subprocess({ args = args, cancellable = false }), args)
+	clip = os.capture(linux_paste, false)
+	return clip
   elseif device == 'windows' then
-    local args = {
-
-      'powershell', '-NoProfile', '-Command', [[& {
-            Trap {
-                Write-Error -ErrorRecord $_
-                Exit 1
-            }
-            $clip = Get-Clipboard -Raw -Format Text -TextFormatType UnicodeText
-			if ($clip) {
-                $clip = $clip
+	if windows_paste == 'powershell' then
+		local args = {
+		  'powershell', '-NoProfile', '-Command', [[& {
+				Trap {
+					Write-Error -ErrorRecord $_
+					Exit 1
 				}
-			else {
-				$clip = Get-Clipboard -Raw -Format FileDropList
-			}
-            $u8clip = [System.Text.Encoding]::UTF8.GetBytes($clip)
-            [Console]::OpenStandardOutput().Write($u8clip, 0, $u8clip.Length)
-        }]]
-    }
-    return handleres(utils.subprocess({ args =  args, cancellable = false }), args)
+				$clip = Get-Clipboard -Raw -Format Text -TextFormatType UnicodeText
+				if ($clip) {
+					$clip = $clip
+					}
+				else {
+					$clip = Get-Clipboard -Raw -Format FileDropList
+				}
+				$u8clip = [System.Text.Encoding]::UTF8.GetBytes($clip)
+				[Console]::OpenStandardOutput().Write($u8clip, 0, $u8clip.Length)
+			}]]
+		}
+		return handleres(utils.subprocess({ args =  args, cancellable = false }), args)
+	else
+		clip = os.capture(windows_paste, false)
+		return clip
+	end
   elseif device == 'mac' then
-    local args = { 'pbpaste' }
-    return handleres(utils.subprocess({ args = args, cancellable = false }), args)
+	clip = os.capture(mac_paste, false)
+	return clip
   end
   return ''
 end
@@ -122,22 +158,28 @@ end
 function set_clipboard(text)
 	local pipe
 	if device == 'linux' then
-		pipe = io.popen("xclip -silent -selection clipboard -in", "w")
+		pipe = io.popen(linux_copy, 'w')
 		pipe:write(text)
 		pipe:close()
-	elseif device == 'windows' then	
-		local res = utils.subprocess({ args = {
-			'powershell', '-NoProfile', '-Command', string.format([[& {
-				Trap {
-					Write-Error -ErrorRecord $_
-					Exit 1
-				}
-				Add-Type -AssemblyName PresentationCore
-				[System.Windows.Clipboard]::SetText('%s')
-			}]], text)
-		} })
+	elseif device == 'windows' then
+		if windows_copy == 'powershell' then
+			local res = utils.subprocess({ args = {
+				'powershell', '-NoProfile', '-Command', string.format([[& {
+					Trap {
+						Write-Error -ErrorRecord $_
+						Exit 1
+					}
+					Add-Type -AssemblyName PresentationCore
+					[System.Windows.Clipboard]::SetText('%s')
+				}]], text)
+			} })
+		else
+			pipe = io.popen(windows_copy,'w')
+			pipe:write(text)
+			pipe:close()
+		end
 	elseif device == 'mac' then
-		pipe = io.popen('pbcopy','w')
+		pipe = io.popen(mac_copy,'w')
 		pipe:write(text)
 		pipe:close()
 	  end
@@ -184,6 +226,8 @@ end
 
 function paste()
 	local clip = get_clipboard()
+	clip = string.gsub(clip, "[\r\n]" , "")
+	
 	local filePath = mp.get_property_native('path')
 	local time
 
@@ -240,7 +284,7 @@ function paste()
 		end
 	elseif (filePath == nil) and (starts_protocol(protocols, videoFile)) then
 		mp.commandv('loadfile', videoFile)
-		mp.osd_message('Pasted URL:\n'..videoFile)
+		mp.osd_message('Pasted:\n'..videoFile)
 		
 		if (time ~= nil) then
 			copyLogAdd:write(('[%s] %s\n'):format(os.date('%d/%b/%y %X'), videoFile..' |time='..tostring(time)))
@@ -279,6 +323,8 @@ end
 
 function paste_playlist()
 	local clip = get_clipboard()
+	clip = string.gsub(clip, "[\r\n]" , "")
+
 	local filePath = mp.get_property_native('path')
 	local time
 	
@@ -335,8 +381,8 @@ mp.register_event('file-loaded', function()
 	end
 end)
 
-
-if device == 'mac' then
+---------------------------KEYBINDS CUSTOMIZATION SETTINGS---------------------------------
+if device == 'mac' then --MAC OS Keybinds
 	mp.add_key_binding('Meta+c', 'copy', copy)
 	mp.add_key_binding('Meta+C', 'copyCaps', copy)
 	mp.add_key_binding('Meta+v', 'paste', paste)
@@ -346,7 +392,7 @@ if device == 'mac' then
 	mp.add_key_binding('Meta+alt+C', 'copy-pathCaps', copy_path)
 	mp.add_key_binding('Meta+alt+v', 'paste-playlist', paste_playlist)
 	mp.add_key_binding('Meta+alt+V', 'paste-playlistCaps', paste_playlist)
-else
+else --Windows and Linux Keybinds
 	mp.add_key_binding('ctrl+c', 'copy', copy)
 	mp.add_key_binding('ctrl+C', 'copyCaps', copy)
 	mp.add_key_binding('ctrl+v', 'paste', paste)
@@ -357,3 +403,4 @@ else
 	mp.add_key_binding('ctrl+alt+v', 'paste-playlist', paste_playlist)
 	mp.add_key_binding('ctrl+alt+V', 'paste-playlistCaps', paste_playlist)
 end
+---------------------END OF KEYBINDS CUSTOMIZATION SETTINGS---------------------------------
