ff2efcd45f3aee2e1b67d908ef2e08e34dbfdb15
dyphire <qimoge@gmail.com>
2022-10-21 15:34:31 +0800

    fix `shared-script-properties` property Commit https://github.com/Eisa01/mpv-scripts/commit/830ffbec0564edc2b5931a04bbab1222da871e2b introduces a error regression Fix it

--- scripts/SmartCopyPaste_II.lua
+++ scripts/SmartCopyPaste_II.lua
@@ -288,7 +288,7 @@ o.open_list_keybind = utils.parse_json(o.open_list_keybind)
 o.list_filter_jump_keybind = utils.parse_json(o.list_filter_jump_keybind)
 o.list_ignored_keybind = utils.parse_json(o.list_ignored_keybind)
 
-utils.shared_script_property_set("menu-open", "no")
+utils.shared_script_property_set("smartcopypaste-menu-open", "no")
 
 if string.lower(o.log_path) == '/:dir%mpvconf%' then
 	o.log_path = mp.find_config_file('.')
