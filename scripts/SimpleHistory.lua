dadaa9ef53b3451931dc22fe862a5ef68f827720
Babil G. Sarwar <gsbabil@gmail.com>
2023-11-25 23:42:32 +1100

    Refactor `nil` checking for `video_duration`
    This commit short-circuits the following check added in the previous commit (d8fcff5) [1]:
    
    ```
        if video_duration and video_duration ~= nil then
          video_duration = tonumber(video_duration)
        else
          video_duration = 0
        end
    ```
    
    [1] https://github.com/gsbabil/eisa01-mpv-scripts/commit/d8fcff58e0767e3fc1fa4c525ee7aee42e06fafa

--- scripts/SimpleHistory.lua
+++ scripts/SimpleHistory.lua
@@ -2115,12 +2115,8 @@ function history_resume_option()
 		if video_time > 0 then return end
 		local logged_time = 0
 		local percentage = 0
-		local video_duration = mp.get_property_number('duration')
-		if video_duration and video_duration ~= nil then
-			video_duration = tonumber(video_duration)
-		else
-			video_duration = 0
-		end	
+		local video_duration = mp.get_property_number('duration') or 0
+
 		list_contents = read_log_table()
 		if not list_contents or not list_contents[1] then return end
 		for i = #list_contents, 1, -1 do
